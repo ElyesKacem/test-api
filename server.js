66a9139b5f4c1a9ce09ddd15ac214508706841de
Hazem Jday <hazemjday@gmail.com>
2023-05-10 17:06:36 +0000

    add firebase

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require("express");
+const admin = require('firebase-admin');
+const serviceAccount = require('./zigbee-3143e-firebase-adminsdk-41isy-dfaa4b1663.json');
 const app = express();
 const axios = require("axios");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://zigbee-3143e-default-rtdb.firebaseio.com/'
+});
+
+const database = admin.database();
+
+setInterval(() => {
+  const temperature = Math.floor(Math.random() * 100);
+  const time = Date.now()
+
+  database.ref('temperatures').push({ temperature, time }, (error) => {
+    if (error) {
+      console.error('Error saving data:', error);
+    } else {
+      console.log(`Temperature ${temperature} saved at ${time}`);
+    }
+  });
+}, 5000);
+
+
+
+
+
+
+
+
 
 app.get("/random", (req, res) => {
   axios.post("https://api.thingspeak.com/update.json", {
@@ -11,6 +41,7 @@ app.get("/random", (req, res) => {
   const randomNumber = Math.floor(Math.random() * (1000 - 500 + 1) + 500); // Generate random number between 500 and 1000
   res.send({ temperature: randomNumber }); // Return the number in JSON format
 });
+
 
 setInterval(() => {
   const randomNumber = Math.floor(Math.random() * (1000 - 500 + 1) + 500); // Generate random number between 500 and 1000
