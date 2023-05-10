e9a3123d5ea3a50705f2d0edd62ed0b569b5ac9c
elyes <elyeskacemeducation@gmail.com>
2023-05-10 17:39:18 +0100

    adding axios to web

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 app.get("/random", (req, res) => {
+  axios.post("https://api.thingspeak.com/update.json", {
+    channel_id: 2140743,
+    field1: 400,
+    api_key: "NK0YWP8IIGT0A2D6",
+  });
   const randomNumber = Math.floor(Math.random() * (1000 - 500 + 1) + 500); // Generate random number between 500 and 1000
   res.send({ temperature: randomNumber }); // Return the number in JSON format
 });
--- temperature.js
+++ temperature.js
@@ -7,8 +7,31 @@ const apiKey = "GVWQQH5H3ZMZLI8M";
 // Generate a random number between 500 and 1000 every 1 second and send it to ThingSpeak
 setInterval(() => {
   const value = Math.floor(Math.random() * 501) + 500;
-  const url = `https://api.thingspeak.com/update?api_key=${apiKey}&field1=${value}`;
+  const url = `https://api.thingspeak.com/update.json?api_key=${apiKey}&field1=${value}`;
   restClient.get(url, (data, response) => {
     console.log(`Data sent to ThingSpeak: ${value}`);
   });
 }, 1000); // Send data every 1 second
+
+// const client = require("node-rest-client").Client;
+
+// // Initialize the REST client with your ThingSpeak API key
+// const restClient = new client();
+// const apiKey = "GVWQQH5H3ZMZLI8M";
+
+// // Generate a random number between 500 and 1000 every 1 second and send it to ThingSpeak using an HTTP POST request
+// setInterval(() => {
+//   const value = Math.floor(Math.random() * 501) + 500;
+//   const data = { api_key: apiKey, field1: value };
+//   const args = {
+//     data,
+//     headers: { "Content-Type": "application/json" },
+//   };
+//   restClient.post(
+//     "https://api.thingspeak.com/2139908/status.json",
+//     args,
+//     (data, response) => {
+//       console.log(`Data sent to ThingSpeak: ${value}`);
+//     }
+//   );
+// }, 1000); // Send data every 1 second
